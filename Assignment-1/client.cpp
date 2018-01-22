475c1e1226fef460b9503e64093581907423c8ce
Additya Popli <additya1998@gmail.com>
2018-01-23 00:31:50 +0530

    fixed error

--- Assignment-1/client.cpp
+++ Assignment-1/client.cpp
@@ -14,6 +14,11 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 
+	if(argc != 2){
+		printf("Usage error \n");
+		return -1;
+	}
+
 	char *fileName = argv[1];
 
 	struct sockaddr_in address;
@@ -44,26 +49,49 @@ int main(int argc, char *argv[]){
 	send(sock, fileName, strlen(fileName), 0);
 	printf("Name of required file sent\n");
 
-	if(read(sock, buffer, 1024) == -1){
+	int l = read(sock, buffer, 1024);
+
+	if(l == -1){
 		printf("Error in getting size of file from server.\n");
 		return -1;
 	}
 
-	int sizeOfFile = atoi(buffer);
+	buffer[l] = '\0';
 
-	if(sizeOfFile == -1){
-		printf("File not found or insufficient privileges to send file on server.\n");
+	if(buffer[0] == '-'){
+		printf("%s", &buffer[3]);
 		return -1;
 	}
 
+	int sizeOfFile = 0, done = 0, s = 0;
+
+	for(int i=0; buffer[i] != '\0'; ++i){
+		if(!done and buffer[i] == '\n') done = i;
+		else if(done){
+			buffer[i - done - 1] = buffer[i];
+			s = i - done;
+		}
+		else sizeOfFile = (sizeOfFile * 10) + buffer[i] - '0';
+	}
+
+	buffer[s] = '\0';
+
 	int fileWrite = open(fileName, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
+	int w = write(fileWrite, buffer, s);
+	int sizeRead = 0;
+
+	if(w == -1){
+		printf("Error in writing to file.\n");
+		return -1;
+	}
+	else sizeRead += w;
+	
 	if(fileWrite == -1){
 		printf("Cannot open file on client.\n");
 		return -1;
 	}
 
-	int sizeRead = 0;
 	while(sizeRead < sizeOfFile){
 		int nowRead = read(sock, buffer, 1024);
 		if(nowRead == -1){
@@ -81,6 +109,7 @@ int main(int argc, char *argv[]){
 	}
 
 	close(fileWrite);
+	close(sock);
 
 	return 0;
 }
